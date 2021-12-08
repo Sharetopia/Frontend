630f9edfe86bacade917ad92f21380c2994ecd2f
Lucas Crämer <lucas.craemer@18pkt1.de>
2021-12-08 12:58:50 +0100

    auth route

--- sharetopia-frontend/src/router/index.ts
+++ sharetopia-frontend/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 import ProductDetail from "../views/ProductDetail.vue";
+import Auth from "../views/Auth.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Auth,
   },
   {
     path: "/productDetail",
