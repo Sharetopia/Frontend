a620b3689ca1dcd88fb645a69d45112f204140f5
Lucas Crämer <lucas.craemer@18pkt1.de>
2021-12-08 11:27:42 +0100

    added amplify

--- sharetopia-frontend/src/main.ts
+++ sharetopia-frontend/src/main.ts
@@ -1,8 +1,22 @@
 import { createApp } from "vue";
+import Amplify from "aws-amplify";
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@aws-amplify/ui-components/loader";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 createApp(App).use(store).use(router).mount("#app");
