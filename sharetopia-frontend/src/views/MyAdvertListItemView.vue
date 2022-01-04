6137bd5eb46f191f5847997333bdf7394299b9e5
js2203 <65500109+js2203@users.noreply.github.com>
2022-01-04 16:16:36 +0100

    picture selecting

--- sharetopia-frontend/src/views/MyAdvertListItemView.vue
+++ sharetopia-frontend/src/views/MyAdvertListItemView.vue
@@ -6,55 +6,9 @@
       image-name="burgerMenu.svg"
       @click="openModal"
     />
-    <TransitionRoot appear :show="isOpen" as="template">
-      <Dialog as="div" @close="closeModal">
-        <div class="fixed inset-0 z-10 overflow-hidden">
-          <div class="min-h-screen px-4 text-center">
-            <TransitionChild
-              as="template"
-              enter="duration-300 ease-out"
-              enter-from="opacity-0"
-              enter-to="opacity-100"
-              leave="duration-200 ease-in"
-              leave-from="opacity-100"
-              leave-to="opacity-0"
-            >
-              <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75" />
-            </TransitionChild>
-
-            <span class="inline-block h-screen" aria-hidden="true">
-              &#8203;
-            </span>
-
-            <TransitionChild
-              as="template"
-              enter="duration-300 ease-out"
-              enter-from="opacity-0 scale-95"
-              enter-to="opacity-100 scale-100"
-              leave="duration-200 ease-in"
-              leave-from="opacity-100 scale-100"
-              leave-to="opacity-0 scale-95"
-            >
-              <div
-                class="
-                  inline-block
-                  w-full
-                  overflow-hidden
-                  text-left
-                  align-middle
-                  transition-all
-                  transform
-                  mt-16
-                "
-              >
-                <!-- Write here the content of the modal -->
-                <AdvertEditComponent :productModel="productModel" />
-              </div>
-            </TransitionChild>
-          </div>
-        </div>
-      </Dialog>
-    </TransitionRoot>
+    <PopUp :is-open="isOpen" @close="closeModal">
+      <AdvertEditComponent :productModel="productModel" />
+    </PopUp>
   </div>
 </template>
 
@@ -70,6 +24,7 @@ import {
   TransitionRoot,
 } from "@headlessui/vue";
 import AdvertEditComponent from "@/components/AdvertEditComponent.vue";
+import PopUp from "@/uiElements/PopUp.vue";
 
 @Options({
   components: {
@@ -80,6 +35,7 @@ import AdvertEditComponent from "@/components/AdvertEditComponent.vue";
     Dialog,
     DialogOverlay,
     AdvertEditComponent,
+    PopUp,
   },
   props: {
     productModel: Object,
