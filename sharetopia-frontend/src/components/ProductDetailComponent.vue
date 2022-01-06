a42b9dd9f66defcd38ffa63ab3aad03b7c99c154
Niklas Schildhauer <ns162@hdm-stuttgart.de>
2022-01-06 14:28:51 +0100

    setup optimization

--- sharetopia-frontend/src/components/ProductDetailComponent.vue
+++ sharetopia-frontend/src/components/ProductDetailComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-if="product.valueOf()" class="flex">
     <div class="w-1/2 px-16">
-      <PhotoGallery />
+      <PhotoGalleryView />
     </div>
     <div class="w-1/2 px-16">
       <div class="divide-y divide-black divide-opacity-25">
         <div class="m-4 mb-8">
-          <ProductTextDetail :productModel="product.valueOf()" />
+          <ProductTextDetailView :productModel="product.valueOf()" />
           <div class="flex mt-8">
             <div class="w-max mr-6">
               <ContactDetail :userId="product.valueOf().userId" />
@@ -15,7 +15,7 @@
               class="h-36 w-full"
               v-if="locationPins.length > 0 && centerPin.valueOf()"
             >
-              <LeafletMap
+              <LeafletMapComponent
                 :pins="locationPins"
                 :center="centerPin.valueOf()"
                 :zoom="15"
@@ -38,50 +38,31 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import ProductTextDetailView from "@/views/ProductTextDetailView.vue"; // @ is an alias to /src
 import LeafletMapComponent from "@/components/LeafletMapComponent.vue";
 import ContactDetail from "@/views/ContactDetailView.vue";
 import PhotoGalleryView from "@/views/PhotoGalleryView.vue";
-import { ProductModel } from "@/model/ProductModel";
+import DatePickerComponent from "@/components/DatePickerComponent.vue";
 import PrimaryButton from "@/uiElements/PrimaryButton.vue";
+import { ProductModel } from "@/model/ProductModel";
 import { watch } from "vue";
 import { useRoute } from "vue-router";
-import DatePickerComponent from "@/components/DatePickerComponent.vue";
 import { useRouteQueries } from "@/composables/useRouteQueries";
 import { useLocationPins } from "@/composables/useLocationPins";
 import { useDatePicker } from "@/composables/useDatePicker";
 import { useProduct } from "@/composables/useProduct";
 
-export default {
-  components: {
-    DatePickerComponent,
-    PrimaryButton,
-    ProductTextDetail: ProductTextDetailView,
-    LeafletMap: LeafletMapComponent,
-    ContactDetail,
-    PhotoGallery: PhotoGalleryView,
-  },
-  setup() {
-    const route = useRoute();
-    const { productId } = useRouteQueries(route.query);
-    const { product } = useProduct(productId);
-    const { locationPins, centerPin, createLocationPins } = useLocationPins();
-    const { datePickerModel, updateDatePickerModel } = useDatePicker();
+const route = useRoute();
+const { productId } = useRouteQueries(route.query);
+const { product } = useProduct(productId);
+const { locationPins, centerPin, createLocationPins } = useLocationPins();
+const { datePickerModel, updateDatePickerModel } = useDatePicker();
 
-    watch(product, (newValue) => {
-      if (newValue) {
-        createLocationPins([newValue as ProductModel]);
-        updateDatePickerModel(newValue as ProductModel);
-      }
-    });
-
-    return {
-      product,
-      locationPins,
-      centerPin,
-      datePickerModel,
-    };
-  },
-};
+watch(product, (newValue) => {
+  if (newValue) {
+    createLocationPins([newValue as ProductModel]);
+    updateDatePickerModel(newValue as ProductModel);
+  }
+});
 </script>
