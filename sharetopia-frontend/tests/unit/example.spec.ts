ba030cba2be6812e94b0bc5531f4057c4d1d8313
js2203 <65500109+js2203@users.noreply.github.com>
2022-02-13 13:37:38 +0100

    replace chai with jest

--- sharetopia-frontend/tests/unit/example.spec.ts
+++ sharetopia-frontend/tests/unit/example.spec.ts
@@ -11,3 +11,15 @@
 //     expect(wrapper.text()).to.include(msg);
 //   });
 // });
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "../../src/components/HelloWorld.vue";
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
